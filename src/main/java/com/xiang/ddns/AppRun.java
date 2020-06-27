e9a4340d47c9d90254d22e16f498c3d155ec45dd
1971119509@qq.com <1971119509@qq.com>
2020-06-27 21:19:53 +0800

    java定时任务

--- src/main/java/com/xiang/ddns/AppRun.java
+++ src/main/java/com/xiang/ddns/AppRun.java
@@ -1,5 +1,8 @@
 package com.xiang.ddns;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * 运行时入口
  * @author xiang
@@ -8,6 +11,15 @@ package com.xiang.ddns;
 public class AppRun {
 
 	public static void main(String[] args) {
+		new Timer("testTimer").schedule(new TimerTask() {
+			@Override
+			public void run() {
+				ddns();
+			}
+		}, 1000,10000);
+	}
+
+	public static void ddns(){
 		System.out.println("开始ddns检查");
 		UpdateDomainRecord record = new UpdateDomainRecord();
 		record.myAnalysisDns();
